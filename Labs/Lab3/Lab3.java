f4f8a1da4ddfd55ba80317742938c89152294b88
Matthew <mdombros@u.rochester.edu>
2016-01-27 22:09:19 -0500

    Part 1 done

--- Labs/Lab3/Lab3.java
+++ Labs/Lab3/Lab3.java
@@ -1,35 +1,34 @@
 import java.util.*;
 public class Lab3 {
   public static void main(String[] args) {
-    System.out.printf("The letters of error can be arranged %d ways\n", nInkBins("Mississippi"));
+    System.out.printf("The letters of Mississippi can be arranged %d ways\n", nInkBins("Mississippi"));
   }
 
   public static int nInkBins(String word) { // n items over k groups
     int n = word.length();
     List<Character> bins = new ArrayList<>();
+    List<Integer> numOfBin = new ArrayList<>();
     for (int i = 0; i < n; i++) {
-      if (bins.contains(word.charAt(i)) == true) {
-        continue;
+      if (bins.contains(word.charAt(i)) == true) {   // Has letter been seen before?
+        numOfBin.set(bins.indexOf(word.charAt(i)), numOfBin.get(bins.indexOf(word.charAt(i)))+1); // If so count it in correct group
       }
-      else {
+      else {  // add letter and instances to lists
         bins.add(word.charAt(i));
-        System.out.println(bins.size());
+        numOfBin.add(1);
       }
     }
     int denom = 1;
-
-    System.out.println(n+bins.size()-1 + " " + Integer.toString(bins.size()-1));
-    return unorderedSelections(n+bins.size()-1, bins.size()-1);
-    //return 3;
+    for (int k = 0; k < numOfBin.size(); k++) {
+      denom *= factorial(numOfBin.get(k));
+    }
+    return factorial(n)/denom;
   }
 
-  public static int unorderedSelections(int objects, int items) {
-    if (items == objects || items == 0){
-        return 1;   // choosing m items from n==m objects has only 1 selection
-                    // choosing 0 items from n objects has only 1 selection
-    }
-    else {
-      return unorderedSelections(objects-1,items)+unorderedSelections(objects-1, items-1);
+  public static int factorial(int n) {
+    if (n == 0) {
+      return 1;
+    } else {
+      return n == 1 ? 1 : n * factorial(n - 1);
     }
   }
 }
