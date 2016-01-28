b1dd864dd56c856d10aa3abdbc3e9c8cb0c19a8c
Matthew <mdombros@u.rochester.edu>
2016-01-27 21:09:54 -0500

    Lab 3 stuff

--- Labs/Lab3/Lab3.java
+++ Labs/Lab3/Lab3.java
@@ -1,29 +1,31 @@
 import java.util.*;
 public class Lab3 {
-  public static void main(Stringp[] args) {
-
+  public static void main(String[] args) {
+    System.out.printf("The letters of error can be arranged %d ways\n", nInkBins("Mississippi"));
   }
 
-  public static orderedItems(String[] word) { // n items over k groups
-    int n = word.length;
-    List bins = new ArrayListp<char>();
+  public static int nInkBins(String word) { // n items over k groups
+    int n = word.length();
+    List<Character> bins = new ArrayList<>();
     for (int i = 0; i < n; i++) {
-      if (word.getChar(i))
+      if (bins.contains(word.charAt(i)) == true) {
+        continue;
+      }
+      else {
+        bins.add(word.charAt(i));
+        System.out.println(bins.size());
+      }
     }
-    return unorderedSelections(n+k-1, k-1);
-  }
-
-  public static int factorial(int n) {
-    if (n == 0) {
-      return 1;
-    } else {
-      return n == 1 ? 1 : n * factorial(n - 1);
-    }
+    int denom = 1;
+    
+    System.out.println(n+bins.size()-1 + " " + Integer.toString(bins.size()-1));
+    return unorderedSelections(n+bins.size()-1, bins.size()-1);
+    //return 3;
   }
 
   public static int unorderedSelections(int objects, int items) {
     if (items == objects || items == 0){
-        return 1;  // choosing m items from n==m objects has only 1 selection
+        return 1;   // choosing m items from n==m objects has only 1 selection
                     // choosing 0 items from n objects has only 1 selection
     }
     else {
